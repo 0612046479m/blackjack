431a5c34e7ca1b6ac90c2034abac0612ec5e1e1d
0612046479m <92538562+0612046479m@users.noreply.github.com>
2024-04-06 16:42:38 +0000

    Co-authored-by: Mason M. Miller <Student> <Mason.Miller@my.browardschools.com> Co-authored-by: Lotus <Blak-Lotus@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
+import os
 from tkinter import *
 from cards import *
 from player import *
 
-txtColor = "#F5F5F5"
-highlight = "#F99417"
-btnColor = "#4D4C7D"
-border = "#3A3845"
-background = "#363062"
+
+txtColor = "#353535"
+hoverColor = "#bdbbb0"
+baseColor = "#8a897c"
+background = "#91ADC2"
 
 root = Tk()
-root.title("Jacle")
+root.title("Jackle Jack")
+root.configure(background=background)
+root.geometry("800x800")
+
+def onHover(e):
+    e.widget["background"] = hoverColor
+    e.widget["foreground"] = txtColor
+
+
+def offHover(e):
+    e.widget["background"] = hoverColor
+    e.widget["foreground"] = txtColor
+
+
+uninstallProgram = Button(
+    root,
+    text="Uninstall Programs",
+    background=background,
+    foreground=txtColor,
+    activebackground=hoverColor,
+    activeforeground=txtColor,
+    border=0,
+    highlightthickness=2,
+    cursor="hand2",
+)
+
+root.mainloop() 
